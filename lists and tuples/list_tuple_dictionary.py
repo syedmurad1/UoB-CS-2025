5c216bd0eb1adf6931c57558e6a1bfe3192a7917
syedmurad1 <89536015+syedmurad1@users.noreply.github.com>
2025-10-22 19:00:51 +0100

    commit

--- lists and tuples/list_tuple_dictionary.py
+++ lists and tuples/list_tuple_dictionary.py
@@ -4,8 +4,10 @@ tuple = (1, 5, 7, 9, 3, 33)
 
 print(tuple[2])
 
+# set = {1, 5, 7, 9, 3, 33}
+# a dictionary is a collection of key-value pairs. Changeable and unordered. No duplicate members. {key1: value1, key2: value2}
+dictionary_student = {"ID": "1", "name": "Tom","age": 22, "dep": "Computer Science"} 
 
-dictionary_student = {"ID": "1", "name": "Tom","age": 22, "dep": "Computer Science"}
 
 print(dictionary_student)
 # print(sdictionary_student["ID"])
@@ -19,3 +21,25 @@ print(staff.get("Tom"))
 
 print(dir(staff))
 print(help(staff))
+
+
+# list[0]=10
+# print(list)
+# list.remove(3)
+# print(list)
+# print(len(list))
+# print(33 in list)
+# print(list.index(9))
+
+
+
+capitals = {"UK": "London",
+            "South Korea": "Seoul",
+            "China": "Beijing",
+            "Russia": "Moscow",
+            "Japan": "Tokyo"
+            }
+
+print(dir(capitals))
+print(help(capitals))
+print(capitals.get("Japan"))
