5625c310853305dbb5bb1c428fa1babd9d6315bc
syed <murad6000@yahoo.com>
2025-10-23 13:38:44 +0100

    commit

--- lists and tuples/try.py
+++ lists and tuples/try.py
@@ -1,13 +1,20 @@
-tuple1=(55,7,30,40)
-a, b, c, d = tuple1
-print(a)
-print(b)
-print(c)
-print(d)
+# tuple1=(55,7,30,40)
+# a, b, c, d = tuple1
+# print(a)
+# print(b)
+# print(c)
+# print(d)
 
-tuple2 = tuple1[1:3]
-print(tuple2)
+# tuple2 = tuple1[1:3]
+# print(tuple2)
 
-tuple2 = tuple1[1], tuple1[2]
+# tuple2 = tuple1[1], tuple1[2]
 
-print(tuple2)
+# print(tuple2)
+
+tom=['A','B','C']
+print(tom[0:])
+print(tom[1])
+print(tom[1:2])
+# print(tom[3])
+# print(tom.index('A'))
