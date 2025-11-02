1c9ea2a0517a1fc9a7cd1f147e443eb79dab4003
syedmurad1 <89536015+syedmurad1@users.noreply.github.com>
2025-11-02 15:54:23 +0000

    commit

--- Input, Output and Import/formatted_output.py
+++ Input, Output and Import/formatted_output.py
@@ -1,9 +1,23 @@
+# # Older Formatting Methods
+# print("Name: {}, Age: {}".format(name, age)) # placeholder -{}
+
+
 # Using f-strings (Python 3.6+)
 # Simplifies string formatting by embedding expressions in {}
+
 name = "Tom"
 age = 20
 print(f"Name: {name}, Age: {age}")
+print(f"Name: {name.upper}, Age: {age}")
 
 
-# Older Formatting Methods
-print("Name: {}, Age: {}".format(name, age)) # placeholder -{}
+
+student={"id":[1,2],"name": ["Tom","Jerry"], "age": [20,22]}
+print(f"ID: {student['id'][0]}, Name: {student['name'][0]}, Age: {student['age'][0]}")
+print(f"ID: {student['id'][1]}, Name: {student['name'][1]}, Age: {student['age'][1]}")
+
+#print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}")
+
+# for i in range(len(student["id"])):
+#     print(f"ID: {student['id'][i]}, Name: {student['name'][i]}, Age: {student['age'][i]}")# print("Name: %s, Age: %d" % (name, age)) # placeholder -%s, %d
+# # print("Name: %s, Age: %d" % (name, age)) # placeholder -%s, %d    
