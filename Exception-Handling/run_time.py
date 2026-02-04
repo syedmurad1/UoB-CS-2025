add276079d3cbb6b66da68a1629ee25ba4e810f5
syedmurad1 <89536015+syedmurad1@users.noreply.github.com>
2026-02-04 17:25:38 +0000

    commit

---
+++ Exception-Handling/run_time.py
@@ -0,0 +1,36 @@
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+
+
+result = a / b
+print("The result is:", result)
+
+
+
+
+
+
+
+# try:
+#     result = a / b
+#     print("The result is:", result)
+# except ZeroDivisionError:
+#     print("Error: Division by zero is not allowed.")
+# except ValueError:
+#     print("Error: Invalid input. Please enter numeric values.")
+# except Exception as e:
+#     print("An unexpected error occurred:", str(e))
+# # The input() function allows users to take input from the console
+# finally:
+#     print("The program execution is complete.")
+
+
+
+
+
+
+
+
+
+
---
+++ Exception-Handling/try.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+print("Hellooo, " + name)
+
+age = int(input("Enter your age: "))
+weight = float(input("Enter your weight: "))
+print("Age:", age, "Weight:", weight)
